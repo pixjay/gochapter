c90345cbe0f155b59cbc805463c7c963bd794a40
Jay Henry <jhenry@pixability.com>
2020-10-05 13:19:21 -0400

    Chapter 4: Modules

--- chapter2/dealer/dealer.go
+++ chapter2/dealer/dealer.go
@@ -1,113 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
 	"time"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"sync"
 	"strconv"
+	"fmt"
+	"github.com/pixjay/gochapter/chapter2/deck"
 )
-
-// Suit of a playing card
-type Suit string
-
-var suits = []Suit{
-	"Spades",
-	"Hearts",
-	"Clubs",
-	"Diamonds",
-}
-
-// FaceValue is the face value of a playing card
-type FaceValue string
-
-var faceValues = []FaceValue{
-	"Ace",
-	"Two",
-	"Three",
-	"Four",
-	"Five",
-	"Six",
-	"Seven",
-	"Eight",
-	"Nine",
-	"Ten",
-	"Jack",
-	"Queen",
-	"King",
-}
-
-// PlayingCard represents a typical playing card with Suit and FaceValue
-type PlayingCard struct {
-	faceValue FaceValue
-	suit      Suit
-}
-
-// Deck of 52 playing cards
-type Deck struct {
-	cards [52]PlayingCard
-	index int
-}
-
-// NewDeck generates a new Deck of 52 PlayingCards
-func NewDeck() *Deck {
-	var deck Deck
-	for i, suit := range suits {
-		for j, faceValue := range faceValues {
-			var card PlayingCard
-			card.suit = suit
-			card.faceValue = faceValue
-			deck.cards[i*len(faceValues)+j] = card
-		}
-	}
-	return &deck
-}
-
-// Shuffle randomizes the order of the PlayingCards in the Deck
-// The algorithm is the Fisher–Yates shuffle, see https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
-// It chooses a random card to place at each index from cards not already chosen, liking pulling cards from a hat
-func (deck *Deck) Shuffle() {
-	deck.index = 0
-	for i := range deck.cards {
-		swap := rand.Intn(len(deck.cards) - i)
-		deck.cards[i], deck.cards[swap + i] = deck.cards[swap + i], deck.cards[i]
-	}
-}
-
-// DealOneCard returns a PlayingCard off the top of the Deck
-func (deck *Deck) DealOneCard() PlayingCard {
-	card := deck.cards[deck.index]
-	deck.index++
-	return card
-}
-
-func (deck Deck) cardsRemaining() int {
-	return len(deck.cards) - deck.index
-}
-
-// String representation of a PlayingCard
-func (card PlayingCard) String() string {
-	return fmt.Sprintf("%v of %v", card.faceValue, card.suit)
-}
 
 type player struct {
 	id int
 	name string
-	hand []PlayingCard
+	hand []deck.PlayingCard
 }
 
 type game struct {
 	id int
-	deck Deck
+	deck deck.Deck
 	players []*player
 	mux sync.Mutex
 }
 
 func (game *game) fprintf(w io.Writer) {
-	fmt.Fprintf(w, "Game %v (cards remaining, %v):\n", game.id, game.deck.cardsRemaining())
+	fmt.Fprintf(w, "Game %v (cards remaining, %v):\n", game.id, game.deck.CardsRemaining())
 	for _, player := range game.players {
 		fmt.Fprintf(w, " - %v (id %v) has a %v card hand\n", player.name, player.id, len(player.hand))
 	}
---
+++ chapter2/deck/go.mod
@@ -0,0 +1,3 @@
+module github.com/pixjay/gochapter/chapter2/deck
+
+go 1.14
